3e6685db0ae3a0dd909668b42b73007179f2ce6d
Andrew Herendeen <andrew@aherendeen.com>
2025-11-28 02:26:08 -0700

    Update docker.ps1

--- powershell/functions/docker.ps1
+++ powershell/functions/docker.ps1
@@ -1,24 +1,46 @@
-# Docker Shortcuts
-# Works with Docker Desktop (native or WSL 2 backend)
+# Docker via WSL Ubuntu
+# Routes all Docker commands to Docker Engine running in WSL Ubuntu
 
-function d { docker @args }
-function dc { docker compose @args }
-function dps { docker ps @args }
-function dpsa { docker ps -a }
-function dimg { docker images }
-function dprune { docker system prune -af }
+#region Core Docker commands (via WSL)
+function docker { wsl.exe docker @args }
+function d { wsl.exe docker @args }
+function dc { wsl.exe docker compose @args }
+function dps { wsl.exe docker ps @args }
+function dpsa { wsl.exe docker ps -a }
+function dimg { wsl.exe docker images }
+function dprune { wsl.exe docker system prune -af }
+#endregion
 
-# Container management
-function dlogs { param($c) docker logs -f $c }
-function dexec { param($c) docker exec -it $c /bin/bash }
-function dsh { param($c) docker exec -it $c /bin/sh }
-function dstop { docker stop $(docker ps -q) }
-function drm { docker rm $(docker ps -aq) }
-function drmi { docker rmi $(docker images -q) }
+#region Container management
+function dlogs { param($c) wsl.exe docker logs -f $c }
+function dexec { param($c) wsl.exe docker exec -it $c /bin/bash }
+function dsh { param($c) wsl.exe docker exec -it $c /bin/sh }
+function dstop { wsl.exe bash -c 'docker stop $(docker ps -q)' }
+function drm { wsl.exe bash -c 'docker rm $(docker ps -aq)' }
+function drmi { wsl.exe bash -c 'docker rmi $(docker images -q)' }
+function dinspect { param($c) wsl.exe docker inspect $c }
+function dstats { wsl.exe docker stats @args }
+#endregion
 
-# Docker Compose shortcuts
-function dcu { docker compose up -d @args }
-function dcd { docker compose down @args }
-function dcl { docker compose logs -f @args }
-function dcr { docker compose restart @args }
-function dcb { docker compose build @args }
+#region Docker Compose shortcuts
+function dcu { wsl.exe docker compose up -d @args }
+function dcd { wsl.exe docker compose down @args }
+function dcl { wsl.exe docker compose logs -f @args }
+function dcr { wsl.exe docker compose restart @args }
+function dcb { wsl.exe docker compose build @args }
+function dce { wsl.exe docker compose exec @args }
+function dcps { wsl.exe docker compose ps }
+#endregion
+
+#region Docker context and info
+function dinfo { wsl.exe docker info }
+function dver { wsl.exe docker version }
+function ddf { wsl.exe docker system df }
+#endregion
+
+#region Volume and network management
+function dvls { wsl.exe docker volume ls }
+function dnls { wsl.exe docker network ls }
+function dvrm { param($v) wsl.exe docker volume rm $v }
+function dnrm { param($n) wsl.exe docker network rm $n }
+#endregion
